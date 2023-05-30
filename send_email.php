d79e969fa09b30cf185101671f492894d4fd005a
Fatima Naveed <fatimanaveed201@gmail.com>
2023-05-30 13:07:28 +0400

    changes

--- send_email.php
+++ send_email.php
@@ -1,27 +1,27 @@
 <?php
-// Set your email address
+// Setting my email address
 $toEmail = 'fatimanaveed201@gmail.com';
 
-// Get form data
+// recieving form data
 $name = $_POST['name'];
 $email = $_POST['email'];
 $message = $_POST['message'];
 
-// Set email headers
-$headers = "From: $name <$email>" . "\r\n" .
-           "Reply-To: $email" . "\r\n" .
-           "X-Mailer: PHP/" . phpversion();
+// Setting email headers
+$headers = "From: $name <$email>" . "\r\n" .   //  code to Set the "From" field to the name and email provided in the form
+           "Reply-To: $email" . "\r\n" .      // code to Set the "Reply-To" field to the email provided in the form
+           "X-Mailer: PHP/" . phpversion();    // code to Add information about the mailer
 
-// Send email
-$mailSent = mail($toEmail, 'New Contact Form Submission', $message, $headers);
+// Sending the email
+$mailSent = mail($toEmail, 'New Contact Form Submission', $message, $headers);   // Send the email with the provided data
 
 if ($mailSent) {
   // Return success response
-  http_response_code(200);
-  echo 'Email sent successfully';
+  http_response_code(200);    // code to Set the HTTP response code to 200 (OK)
+  echo 'Email sent successfully';   //code to Display a success message
 } else {
   // Return error response
-  http_response_code(500);
-  echo 'Unable to send email';
+  http_response_code(500);    //  code to Set the HTTP response code to 500 (Internal Server Error)
+  echo 'Unable to send email';    //  code to Display an error message
 }
 ?>
